a0b9f1e2df056c4f8945eebfc8bc5a46e1c8cf95
Marcos001 <santosMsantos01@gmail.com>
2017-10-24 20:40:42 -0300

    Test no Consumer

---
+++ src/com/lippo/marcos/connection/ConsumidorTest.java
@@ -0,0 +1,10 @@
+package com.lippo.marcos.connection;
+
+import static org.junit.Assert.*;
+
+public class ConsumidorTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+}
